4e0b68bdf6d8ce5564eae3b66c9be9e73c951586
Biraj Thapa <birajthapa983@gmail.com>
2025-02-28 11:29:59 +0545

    get all user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -34,3 +34,12 @@ export const login = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+export const users = async (req, res) => {
+  try {
+    const user =await User.find();
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { register,login } from "../controllers/user.js";
+import { register,login,users } from "../controllers/user.js";
 
 const router=express.Router()
 
 router.post("/register",register)
 router.post("/login",login)
+router.get("/users",users)
 
 
 export default router
