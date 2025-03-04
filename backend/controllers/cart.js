a4429e5c29de62b494079092ed59d2be5ce162c8
Biraj Thapa <birajthapa983@gmail.com>
2025-03-04 20:30:43 +0545

    decrease cart quantity

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -54,11 +54,73 @@ export const removeProductFromCart = async (req, res) => {
 
     if (!cart) return res.json({ message: "cart not found" });
 
-    cart.items=cart.items.filter((item)=>item.productId.toString() !== productId)
+    cart.items = cart.items.filter(
+      (item) => item.productId.toString() !== productId
+    );
 
-    await cart.save()
+    await cart.save();
 
-    res.json({ message: "product remove from cart"});
+    res.json({ message: "product remove from cart" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const clearCart = async (req, res) => {
+  try {
+    const userId = "67c3cb3b580b5dba7c948baa";
+
+    let cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      cart = new Cart({ items: [] });
+    } else {
+      cart.items = [];
+    }
+
+    await cart.save();
+
+    res.json({ message: "cart cleared" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const decreaseProductQty = async (req, res) => {
+  try {
+    const { productId, qty } = req.body;
+
+    const userId = "67c3cb3b580b5dba7c948baa";
+
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+    if (itemIndex > -1) {
+      const item = cart.items[itemIndex];
+
+      if (item.qty > qty) {
+        const pricePerUnit = item.price / item.qty;
+        item.qty -= qty;
+        item.price -= pricePerUnit * qty;
+      }
+      else{
+        cart.items.splice(itemIndex,1)
+      }
+
+    } else {
+      return res.json({message:"Invalid product Id"})
+    }
+    await cart.save();
+    res.status(200).json({
+      success: true,
+      message: "Item qty decreased ",
+      cart,
+    });
   } catch (err) {
     res.status(500).json({ success: false, message: "Internal server error" });
   }
--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addToCart, removeProductFromCart, userCart } from "../controllers/cart.js";
+import { addToCart, clearCart, decreaseProductQty, removeProductFromCart, userCart } from "../controllers/cart.js";
 const router=express.Router()
 
 router.post("/add",addToCart)
 router.get("/user",userCart)
 router.delete("/remove/:productId",removeProductFromCart)
+router.delete("/clear",clearCart)
+router.post("/--qty",decreaseProductQty)
 
 export default router
