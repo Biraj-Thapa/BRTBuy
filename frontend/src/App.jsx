485fbae720c2bb214cb131cbe894327a95682947
Biraj Thapa <birajthapa983@gmail.com>
2025-03-09 21:47:16 +0545

    product detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import React, { useContext } from 'react'
-import AppContext from './context/AppContext'
-import ShowProduct from "./components/product/ShowProduct"
+import React, { useContext } from "react";
+import AppContext from "./context/AppContext";
+import ShowProduct from "./components/product/ShowProduct";
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+import ProductDetail from "./components/product/ProductDetail";
 
 const App = () => {
- 
   return (
     <>
-    <ShowProduct/>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<ShowProduct />}/>
+          <Route path="/product/:id" element={<ProductDetail/>} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
